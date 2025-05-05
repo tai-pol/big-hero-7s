4a845d339903d83b1f5a9f1e15c4af283955393b
Renata Zurita <renatazurita9@gmail.com>
2025-05-04 19:00:59 -0600

    cv edit

--- final_project-2/controllers/grocery_shopper/cv.py
+++ final_project-2/controllers/grocery_shopper/cv.py
@@ -80,72 +80,6 @@ K = np.array([
     [0, 0, 1]
 ])
 
-# def run_cv(camera,depth_cam, yaw, pitch):
-#     CONF = .25 
-#     raw = camera.getImage()
-#     bgra  = np.frombuffer(raw, np.uint8).reshape((CAMERA_HEIGHT, CAMERA_WIDTH, 4))
-#     bgr   = cv2.cvtColor(bgra, cv2.COLOR_BGRA2BGR)
-
-#     result   = model.predict(bgr, conf=CONF, verbose=False)[0]
-#     found    = False
-
-#     for box in result.boxes:
-#         if int(box.cls[0]) != CUBE_ID:
-#             continue
-#         found = True
-#         # topleft and bottomright
-#         x1,y1,x2,y2 = map(int, box.xyxy[0]); 
-
-#         # center of found cube
-#         cx,cy = (x1+x2)//2, (y1+y2)//2
-
-#         cv2.rectangle(bgr,(x1,y1),(x2,y2),(0,255,0),2)
-#         cv2.circle(bgr,(cx,cy),4,(0,0,255),-1)
-#         cv2.circle(bgr, (cx, cy), 4, (255, 0, 255), 2)
-
-#         # normalize to get center of cube 
-#         u = (cx + 0.5) / CAMERA_WIDTH
-#         v = (cy + 0.5) / CAMERA_HEIGHT 
-
-#         # scale horizontal and vert position in depth image
-#         cx_s = u * depth_W
-#         cy_s = v * depth_H
-
-#         # rotate 90
-#         cx_d = int(depth_H - 1 - cy_s) 
-#         cy_d = int(cx_s) 
-
-#         # clamp
-#         cx_d = max(0, min(depth_W  - 1, cx_d))
-#         cy_d = max(0, min(depth_H - 1, cy_d))
-
-
-#         # normalized coords -1 - 1
-#         nx = (cx - CAMERA_WIDTH/2)  / (CAMERA_WIDTH/2)
-#         ny = (cy - CAMERA_HEIGHT/2) / (CAMERA_HEIGHT/2)
-
-#         depth_img = depth_cam.getRangeImageArray()
-#         valid_depths = [d for row in depth_img for d in row if not math.isinf(d) and d > 0.01]
-#         print(f"Depth map: {len(valid_depths)} valid values, median = {np.median(valid_depths):.2f} m")
-#         depth = depth_img[cy_d][cx_d]  
-
-
-#         if math.isinf(depth) or depth <= 0.0:
-#             continue
-
-#         x_c = depth
-#         y_c = -nx * depth * np.tan(FIELD_OF_VIEW/2)
-#         z_c = -ny * depth * np.tan(FIELD_OF_VIEW/2)
-#         pt_cam  = np.array([x_c, y_c, z_c, 1.0])
-
-        
-#     if found:
-#         cv2.imshow("Cube Detection", bgr)
-        
-#         cv2.waitKey(1)
-    
-#     else:
-#         return
     
 def run_cv(camera, depth_cam):
     raw = camera.getImage()
@@ -186,119 +120,104 @@ def run_cv(camera, depth_cam):
         z_c = -ny * depth * np.tan(FIELD_OF_VIEW/2)
         pt_cam = np.array([x_c, y_c, z_c, 1.0])
 
-        Rz = np.array([
-            [ np.cos(yaw), 0, np.sin(yaw), 0],
-            [ 0,           1, 0,           0],
-            [-np.sin(yaw), 0, np.cos(yaw), 0],
-            [ 0,           0, 0,           1]
-        ])
-        Ry = np.array([
-            [ 1, 0,            0,             0],
-            [ 0, np.cos(pitch),-np.sin(pitch), 0],
-            [ 0, np.sin(pitch), np.cos(pitch), 0],
-            [ 0, 0,            0,             1]
-        ])
-        T_base_cam = T_base_pan @ Rz @ Ry @ T_tilt_cam
-        pt_base = T_base_cam @ pt_cam
-        print("Cube in robot frame:", np.round(pt_base[:3], 3))
 
         cv2.imshow("Cube Detection", bgr)
         cv2.waitKey(1)
-        return True, pt_base[:3]
+        return True
 
     return False, None
 
 
-print("Press **F** to scan the current frame for cubes…")
-while robot.step(timestep) != -1:
-    yaw   = yaw_sensor.getValue()     # radians
-    pitch = pitch_sensor.getValue() 
+# print("Press **F** to scan the current frame for cubes…")
+# while robot.step(timestep) != -1:
+#     yaw   = yaw_sensor.getValue()     # radians
+#     pitch = pitch_sensor.getValue() 
 
-    key = kbd.getKey()
+#     key = kbd.getKey()
 
-    if key not in (ord('F'), ord('f')):
-        continue
-
-
-    raw   = camera.getImage()
-    bgra  = np.frombuffer(raw, np.uint8).reshape((CAMERA_HEIGHT, CAMERA_WIDTH, 4))
-    bgr   = cv2.cvtColor(bgra, cv2.COLOR_BGRA2BGR)
-
-    result   = model.predict(bgr, conf=CONF, verbose=False)[0]
-    found    = False
-
-    for box in result.boxes:
-        if int(box.cls[0]) != CUBE_ID:
-            continue
-        found = True
-        # topleft and bottomright
-        x1,y1,x2,y2 = map(int, box.xyxy[0]); 
-
-        # center of found cube
-        cx,cy = (x1+x2)//2, (y1+y2)//2
-
-        cv2.rectangle(bgr,(x1,y1),(x2,y2),(0,255,0),2)
-        cv2.circle(bgr,(cx,cy),4,(0,0,255),-1)
-        cv2.circle(bgr, (cx, cy), 4, (255, 0, 255), 2)
-
-        # normalize to get center of cube 
-        u = (cx + 0.5) / CAMERA_WIDTH
-        v = (cy + 0.5) / CAMERA_HEIGHT 
-
-        # scale horizontal and vert position in depth image
-        cx_s = u * depth_W
-        cy_s = v * depth_H
-
-        # rotate 90
-        cx_d = int(depth_H - 1 - cy_s) 
-        cy_d = int(cx_s) 
-
-        # clamp
-        cx_d = max(0, min(depth_W  - 1, cx_d))
-        cy_d = max(0, min(depth_H - 1, cy_d))
-
-        print(f"RGB centre ({cx},{cy})  ->  depth index ({cx_d},{cy_d})")
-
-        # normalized coords -1 - 1
-        nx = (cx - CAMERA_WIDTH/2)  / (CAMERA_WIDTH/2)
-        ny = (cy - CAMERA_HEIGHT/2) / (CAMERA_HEIGHT/2)
-
-        yaw   = yaw_sensor.getValue()
-        pitch = pitch_sensor.getValue()
-
-        Rz = np.array([[ np.cos(yaw), 0, np.sin(yaw), 0],
-                    [ 0,           1, 0,           0],
-                    [-np.sin(yaw), 0, np.cos(yaw), 0],
-                    [ 0,           0, 0,           1]])
-        Ry = np.array([[ 1, 0,            0,             0],
-                    [ 0, np.cos(pitch),-np.sin(pitch), 0],
-                    [ 0, np.sin(pitch), np.cos(pitch), 0],
-                    [ 0, 0,            0,             1]])
-        T_base_cam = T_base_pan @ Rz @ Ry @ T_tilt_cam
+#     if key not in (ord('F'), ord('f')):
+#         continue
 
 
-        depth_img = depth_cam.getRangeImageArray()
-        valid_depths = [d for row in depth_img for d in row if not math.isinf(d) and d > 0.01]
-        print(f"Depth map: {len(valid_depths)} valid values, median = {np.median(valid_depths):.2f} m")
-        depth = depth_img[cy_d][cx_d]  
+#     raw   = camera.getImage()
+#     bgra  = np.frombuffer(raw, np.uint8).reshape((CAMERA_HEIGHT, CAMERA_WIDTH, 4))
+#     bgr   = cv2.cvtColor(bgra, cv2.COLOR_BGRA2BGR)
+
+#     result   = model.predict(bgr, conf=CONF, verbose=False)[0]
+#     found    = False
+
+#     for box in result.boxes:
+#         if int(box.cls[0]) != CUBE_ID:
+#             continue
+#         found = True
+#         # topleft and bottomright
+#         x1,y1,x2,y2 = map(int, box.xyxy[0]); 
+
+#         # center of found cube
+#         cx,cy = (x1+x2)//2, (y1+y2)//2
+
+#         cv2.rectangle(bgr,(x1,y1),(x2,y2),(0,255,0),2)
+#         cv2.circle(bgr,(cx,cy),4,(0,0,255),-1)
+#         cv2.circle(bgr, (cx, cy), 4, (255, 0, 255), 2)
+
+#         # normalize to get center of cube 
+#         u = (cx + 0.5) / CAMERA_WIDTH
+#         v = (cy + 0.5) / CAMERA_HEIGHT 
+
+#         # scale horizontal and vert position in depth image
+#         cx_s = u * depth_W
+#         cy_s = v * depth_H
+
+#         # rotate 90
+#         cx_d = int(depth_H - 1 - cy_s) 
+#         cy_d = int(cx_s) 
+
+#         # clamp
+#         cx_d = max(0, min(depth_W  - 1, cx_d))
+#         cy_d = max(0, min(depth_H - 1, cy_d))
+
+#         print(f"RGB centre ({cx},{cy})  ->  depth index ({cx_d},{cy_d})")
+
+#         # normalized coords -1 - 1
+#         nx = (cx - CAMERA_WIDTH/2)  / (CAMERA_WIDTH/2)
+#         ny = (cy - CAMERA_HEIGHT/2) / (CAMERA_HEIGHT/2)
+
+#         yaw   = yaw_sensor.getValue()
+#         pitch = pitch_sensor.getValue()
+
+#         Rz = np.array([[ np.cos(yaw), 0, np.sin(yaw), 0],
+#                     [ 0,           1, 0,           0],
+#                     [-np.sin(yaw), 0, np.cos(yaw), 0],
+#                     [ 0,           0, 0,           1]])
+#         Ry = np.array([[ 1, 0,            0,             0],
+#                     [ 0, np.cos(pitch),-np.sin(pitch), 0],
+#                     [ 0, np.sin(pitch), np.cos(pitch), 0],
+#                     [ 0, 0,            0,             1]])
+#         T_base_cam = T_base_pan @ Rz @ Ry @ T_tilt_cam
 
 
-        if math.isinf(depth) or depth <= 0.0:
-            continue
+#         depth_img = depth_cam.getRangeImageArray()
+#         valid_depths = [d for row in depth_img for d in row if not math.isinf(d) and d > 0.01]
+#         print(f"Depth map: {len(valid_depths)} valid values, median = {np.median(valid_depths):.2f} m")
+#         depth = depth_img[cy_d][cx_d]  
 
-        x_c = depth
-        y_c = -nx * depth * np.tan(FIELD_OF_VIEW/2)
-        z_c = -ny * depth * np.tan(FIELD_OF_VIEW/2)
-        pt_cam  = np.array([x_c, y_c, z_c, 1.0])
 
-        pt_base = T_base_cam @ pt_cam
-        print("cube in robot frame XYZ:", np.round(pt_base[:3],3))
-    if found:
-        cv2.imshow("Cube Detection", bgr)
+#         if math.isinf(depth) or depth <= 0.0:
+#             continue
+
+#         x_c = depth
+#         y_c = -nx * depth * np.tan(FIELD_OF_VIEW/2)
+#         z_c = -ny * depth * np.tan(FIELD_OF_VIEW/2)
+#         pt_cam  = np.array([x_c, y_c, z_c, 1.0])
+
+#         pt_base = T_base_cam @ pt_cam
+#         print("cube in robot frame XYZ:", np.round(pt_base[:3],3))
+#     if found:
+#         cv2.imshow("Cube Detection", bgr)
         
-        cv2.waitKey(3)
+#         cv2.waitKey(3)
 
-    else:
-        continue
+#     else:
+#         continue
 
-    pass
+#     pass
